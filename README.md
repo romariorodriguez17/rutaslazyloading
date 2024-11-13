f83415ac20178f9edfe51813df1e012a5da1c045
Romario Rodriguez <romariorodriguez17@gmail.com>
2024-11-13 10:59:21 -0500

    feat(backend) Se añade listado de heroes

--- README.md
+++ README.md
@@ -25,3 +25,11 @@ Run `ng e2e` to execute the end-to-end tests via a platform of your choice. To u
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI Overview and Command Reference](https://angular.dev/tools/cli) page.
+
+
+##dev 
+
+1. Clonar el proyecto
+2. Ejecutar ``` npm install```
+3. levantar backend ``` npm run backend```
+4. Ejecutar la app ```npm start``` o bien ```ng serve -o```
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,10 +15,12 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
     BrowserModule,
     AppRoutingModule,
     SharedModule,
+    HttpClientModule,
   ],
   providers: [
     provideClientHydration(),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+    provideHttpClient(),
   ],
   bootstrap: [AppComponent]
 })
