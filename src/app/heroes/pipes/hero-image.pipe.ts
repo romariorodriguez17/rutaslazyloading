953a7e707991cf8785c916066108931f792147a0
Romario Rodriguez <romariorodriguez17@gmail.com>
2024-11-15 14:48:02 -0500

    feat(angular-json)se soluciona el error de image

--- src/app/heroes/pipes/hero-image.pipe.ts
+++ src/app/heroes/pipes/hero-image.pipe.ts
@@ -14,7 +14,7 @@ export class HeroImagePipe implements PipeTransform {
 
     if (hero.alt_img) return hero.alt_img; // https:///google.com/flash.png
 
-    return `/assets/heroes${hero.id}.jpg`;
+    return `assets/heroes/${hero.id}.jpg`;
 
   }
 
